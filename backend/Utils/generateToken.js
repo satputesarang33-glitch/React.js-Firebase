2346eaabb51225f37087b1d224b2a6989928347c
Saran <satputesarang33@gmail.com>
2026-08-28 02:23:17 +0530

    Full

--- backend/Utils/generateToken.js
+++
@@ -1,5 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
-};
--- backend/Utils/slugify.js
+++
@@ -1,9 +0,0 @@
-export function slugify(text) {
-  return value
-    .toString()
-    .trim()
-    .toLowerCase()
-    .replace(/[^a-z0-9-]/g, "-")
-    .replace(/-+/g, "-")
-    .replace(/^-|-$/g, "");
-}
